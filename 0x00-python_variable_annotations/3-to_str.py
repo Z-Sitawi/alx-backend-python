4089c2626849f99cb28e87cfd8195c9dd6bcfcf2
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-07 09:25:17 +0100

    task 3

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" function to_str that takes a float n as argument
+    and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    :param n: a float number
+    :return: the string representation of the float number
+    """
+    return str(n)
