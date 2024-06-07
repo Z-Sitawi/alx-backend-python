a3ea01c62f3760bf0b4ce58ae6a8b141513426b1
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-07 09:00:52 +0100

    task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,9 @@
 
 
 def add(a: float, b: float) -> float:
+    """
+    :param a: first float number
+    :param b: second float number
+    :return: the sum of the two numbers
+    """
     return a + b
