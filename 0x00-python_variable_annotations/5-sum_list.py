f11b652ebf65482621da8a077b2a2172c53f486a
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-07 09:34:10 +0100

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" function sum_list which takes a list input_list of floats as argument
+    and returns their sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+
+    :param input_list: A list of floats
+    :return: the sum of the floats
+    """
+    return float(sum(input_list))
