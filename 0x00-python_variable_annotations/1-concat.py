dc73b9d3c3a2fc107c1edcfccdbe9673ffc4ec21
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-07 09:11:46 +0100

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" function concat that takes a string str1 and a string str2 as arguments
+    and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    :param str1: first str number
+    :param str2: second str number
+    :return: a concatenated string
+    """
+    return str1 + str2
