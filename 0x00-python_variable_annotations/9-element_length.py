40f03237229bb147eb5f51965d621a15a02623ef
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-07 10:24:48 +0100

    task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" module doc """
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """
+    :param lst: list of strings
+    :return: a list of tuples
+    """
+    return [(i, len(i)) for i in lst]
