7f6a71f0ea14da7c28c508f248628dc7c0f993cc
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-07 09:21:33 +0100

    task 2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" function floor which takes a float n as argument
+    and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> float:
+    """
+    :param n: a float number
+    :return: the floor of the float
+    """
+    import math
+    return math.floor(n)
