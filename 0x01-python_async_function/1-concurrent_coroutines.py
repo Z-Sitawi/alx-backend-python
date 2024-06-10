e27ec7e4da6a7ada2220f46922fd6f66020414d5
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-11 00:05:23 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 """ This is where to put the model Documentation"""
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ spawn wait_random n times with the specified max_delay. """
+    coroutines = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*coroutines)
+    return sorted(results)
