9de7d11f839b898326fce70d4adc4b3f911f0eb3
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-11 19:53:21 +0100

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,4 +8,4 @@ async def async_generator():
     """ Here is the coroutine documentation"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 11)
+        yield random.randint(0, 10)
